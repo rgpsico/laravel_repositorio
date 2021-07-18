6f5d43d606209580bbb7392d1573baefcf19fa4b
rgpsico <gerro121@hotmail.com>
2021-07-18 07:33:30 -0300

    primeiro commit

---
+++ laravel/database/migrations/2021_07_18_071946_categories.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Categories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique();
+            $table->string('url')->unique();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title','AdminLTE')
+
+@section('content_header')
+
+@stop 
+
+@section('content')
+    <p>You Are Login</p>
+@stop
